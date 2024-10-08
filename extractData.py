e179e3feeaff31b22983c63d0ee28d642f6a22a2
Aaron Anderson <aaron.m.anderson@outlook.com>
2024-10-08 18:49:24 -0400

    changed file name to reduce confusion

--- extractData.py
+++ extractData.py
@@ -1,5 +1,5 @@
 # This function takes in the workbook and the section of data, and returns a dictionary with the filtered data
-def extract_data(workbook_obj, section_str: str) -> dict:
+def extract_data(workbook_obj, section_str: str) -> list:
     data_dict = {}  # create dict to hold data
 
     rows = []  # create list of lists to hold rows while looping
@@ -12,9 +12,12 @@ def extract_data(workbook_obj, section_str: str) -> dict:
 
         # loop through each cell in the current row
         for cell in row:
+
             # If the cell has a dash make note
             if cell.value == "–":
-                contain_dash = True
+                row_data.append("-")
+
+
             # If the value is a string (The country name) increment string count so other strings will not be accepted
             elif isinstance(cell.value, str):
                 if str_count == 0:
@@ -24,13 +27,10 @@ def extract_data(workbook_obj, section_str: str) -> dict:
             elif isinstance(cell.value, float) or isinstance(cell.value, int):
                 rounded_value = int(round(cell.value))
                 row_data.append(rounded_value)
-        # only append the row if there were no dashes
-        if contain_dash is False:
-            rows.append(row_data)
-
-    # for the amount of rows, add each to the dictionary.
-    for x in range(len(rows)):
-        data_dict[x] = rows[x]
+        # fixes a bug where Andorra was one dash short
+        if row_data[0] == "Andorra":
+            row_data.append("-")
+        rows.append(row_data)
 
     # return the cleaned data
-    return data_dict
+    return rows
